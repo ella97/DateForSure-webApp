cadcdd6a0b1c8db3ba0d18e8d7d66f2e8f6de6c8
noella mwanri <noella@gailnet.co.tz>
2022-04-02 19:31:48 +0300

    Added app-routing module + added angular/material, angular/flex-layout, angular/cdk

--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    RouterModule, 
+    BrowserAnimationsModule, 
+    MatButtonModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
