67846db759dc4023c383d8b4b48519152f2dbf60
noella mwanri <noella@gailnet.co.tz>
2022-04-02 19:50:34 +0300

    Made the default component work

--- dashboard/src/app/app-routing.module.ts
+++ dashboard/src/app/app-routing.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { DefaultComponent } from './layouts/default/default.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
 
 
 const routes: Routes = [
 
   {
     path: '',
-    redirectTo: 'welcome',
-    pathMatch: 'full'
+    component: DefaultComponent,
+    children: [{
+        path: '',
+        component: DashboardComponent
+    }]
   },
-  
 ];
 
 
--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -1,25 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
+// import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DefaultComponent } from './layouts/default/default.component';
-import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { DefaultModule } from './layouts/default/default.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DefaultComponent,
-    DashboardComponent
   ],
   imports: [
     BrowserModule, 
     RouterModule, 
     BrowserAnimationsModule, 
-    MatButtonModule,
-    AppRoutingModule
+    // MatButtonModule,
+    AppRoutingModule,
+    DefaultModule
   ],
   providers: [],
   bootstrap: [AppComponent]
